41081bce5aefc0001ca970a21269dd77daab94b9
fsarachu <fsarachu@gmail.com>
2019-01-04 12:46:49 -0300

    Scaffold single question route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post '/questions' => 'home#temp'
 
+  get '/questions/:id' => 'home#question'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
